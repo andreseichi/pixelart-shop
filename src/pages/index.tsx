6665c661afca5fe0446f095fcc137ea8d78eaab2
tashiro <andre_seichi@hotmail.com>
2023-11-03 21:50:49 -0300

    fix stripe list only actice products

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -156,6 +156,7 @@ export default function Home({ products }: HomeProps) {
 export const getStaticProps: GetStaticProps = async () => {
   const response = await stripe.products.list({
     expand: ["data.default_price"],
+    active: true,
   });
 
   const products = response.data.map((product) => {
